5fc43c4b940fdec606522b4ccfd1c7eb6d32a5fe
Turle-art <israelsabela@gmail.com>
2024-05-28 11:27:13 +0200

    Initial commit

---
+++ equinox/src/main/java/com/cloud/equinox/EquinoxApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.equinox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquinoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquinoxApplication.class, args);
+	}
+
+}
---
+++ equinox/src/test/java/com/cloud/equinox/EquinoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.equinox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquinoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
